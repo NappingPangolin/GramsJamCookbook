73d163d51bdced34786550de043b3bdbe408de58
Napping Pangolin <112192918+NappingPangolin@users.noreply.github.com>
2024-01-29 22:21:25 +0100

    Update_QR

--- QR_Codes/qrcode_generator.py
+++ QR_Codes/qrcode_generator.py
@@ -9,5 +9,17 @@ def make_qrcode(filename, input, qr_size):
     img = qr.make_image()
     img.save(filename)
 
+qr_path = __file__
 
-make_qrcode('QR_FeedTheMachine.png','https://tabs.ultimate-guitar.com/tab/poor-mans-poison/feed-the-machine-chords-3349439',177)
+
+#Simple Classics
+make_qrcode(qr_path+'QR_FeedTheMachine.png','https://drive.google.com/file/d/1Db6Ug0zLC6MYR0MDvbHU-q7UPug2lBHZ/view?usp=sharing',177)
+make_qrcode(qr_path+'QR_SixteenTons_Chords.png','https://drive.google.com/file/d/1Umn86kdy6vKL7eu00424nV5WImphKJd6/view?usp=sharing',177)
+make_qrcode(qr_path+'QR_HitTheRoadJack_Chords.png','https://drive.google.com/file/d/1_DVB0jdnRM9bB043iI_6aLIJ1hE3N0GM/view?usp=sharing',177)
+
+#Rock songs
+make_qrcode(qr_path+'QR_SultansOfSwing_BassChords.png','https://drive.google.com/file/d/14Mgmnunz_iVf0pWnBqPTu4IfZM3BnELL/view?usp=sharing',177)
+
+#Movie and Games
+make_qrcode(qr_path+'QR_LuxAeterna_CheatSheet.png','https://drive.google.com/file/d/1AvHzKg3lPEnnRlelcVWPcTGUGytP3JMC/view?usp=sharing',177)
+
