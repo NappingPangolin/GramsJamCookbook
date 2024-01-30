715c93c56dccb55808b2a8acec494dada6bb755b
Napping Pangolin <112192918+NappingPangolin@users.noreply.github.com>
2024-01-30 13:14:38 +0100

    Updated Many Chapters
    How to blues, Ikarus, Misirlou, Overtherainbow, Yorks, HTRJ, Lux, Sixteen, Sultans, Feed the Machine

--- Latex/QR_Codes/0_qrcode_generator.py
+++ Latex/QR_Codes/0_qrcode_generator.py
@@ -17,9 +17,13 @@ make_qrcode(qr_path+'QR_FeedTheMachine.png','https://drive.google.com/file/d/1Db
 make_qrcode(qr_path+'QR_SixteenTons_Chords.png','https://drive.google.com/file/d/1Umn86kdy6vKL7eu00424nV5WImphKJd6/view?usp=sharing',177)
 make_qrcode(qr_path+'QR_HitTheRoadJack_Chords.png','https://drive.google.com/file/d/1_DVB0jdnRM9bB043iI_6aLIJ1hE3N0GM/view?usp=sharing',177)
 
+#Blues
+make_qrcode(qr_path+'QR_12BarBlues.png','https://drive.google.com/file/d/1OHr9iwjNYFbsxZHoa1RS-AfROFuDm8Ch/view?usp=sharing',177)
+
 #Rock songs
 make_qrcode(qr_path+'QR_SultansOfSwing_BassChords.png','https://drive.google.com/file/d/14Mgmnunz_iVf0pWnBqPTu4IfZM3BnELL/view?usp=sharing',177)
 
 #Movie and Games
 make_qrcode(qr_path+'QR_LuxAeterna_CheatSheet.png','https://drive.google.com/file/d/1AvHzKg3lPEnnRlelcVWPcTGUGytP3JMC/view?usp=sharing',177)
+make_qrcode(qr_path+'QR_LuxAeterna_Chords.png','https://drive.google.com/file/d/1spoS4tFDuOX45NCh1EDfrDmjujJ_5gaF/view?usp=sharing',177)
 
